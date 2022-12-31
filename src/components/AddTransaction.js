01bbcdf29e33eeaa82592259663edf5dd5fea2a9
Anup <anupbasnet057@gmail.com>
2022-12-31 12:17:53 +0545

    can add transactions now

--- src/components/AddTransaction.js
+++ src/components/AddTransaction.js
@@ -1,16 +1,31 @@
 import React from 'react'
-import { useState } from 'react'
+import { useState, useContext } from 'react'
+import { GlobalContext } from '../context/GlobalState';
 
 const AddTransaction = () => {
     const [text, setText] = useState('');
     const [amount, setAmount] = useState(0);
 
+    const { addTransaction } = useContext(GlobalContext);
 
+    const handleSubmit = e => {
+        e.preventDefault();
+
+        const newTransaction = {
+            id: Math.floor(Math.random() * 10000000),
+            text,
+            amount: +amount
+        }
+
+        addTransaction(newTransaction)
+        setText('');
+        setAmount(0);
+    }
 
   return (
     <>
         <h3>Add new transaction</h3>
-        <form>
+        <form onSubmit={handleSubmit}>
             <div className="form-control">
                 <label htmlFor="text">Text</label>
                 <input type="text" 
